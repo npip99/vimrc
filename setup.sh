42a8d9e547b9ffbe37be28c9572f771af7748235
Nicholas Pipitone <npip99@gmail.com>
2016-06-13 19:15:14 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+# Update GCC
+sudo apt-get update
+sudo apt-get install g++-6
+sudo rm /usr/bin/g++                                                                                                              
+sudo ln -s /usr/bin/g++-6 /usr/bin/g++      
+sudo rm /usr/bin/gcc
+sudo ln -s /usr/bin/gcc-6 /usr/bin/gcc
+
+# Setup VIM
 rvm get head
 sudo rvm install 2.3.1
 cp vimrc ~/.vimrc

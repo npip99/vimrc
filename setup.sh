7823e07daf0a4f7695ca65aeaa26f9feb86a1fb8
npip99 <npip99@gmail.com>
2016-06-13 16:17:44 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,11 @@
-cp vimrc ~/.vimrc
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/linuxbrew/go/install)"
-sudo apt-get install build-essential
+#!/bin/sh
 
+rvm get head
+sudo rvm install 2.3.1
+cp vimrc ~/.vimrc
+yes "" | ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/linuxbrew/go/install)"
+sudo apt-get install build-essential
+export LD_LIBRARY_PATH="/usr/local/rvm/rubies/ruby-2.3.1/lib:$LD_LIBRARY_PATH"
 export PATH="$HOME/.linuxbrew/bin:$PATH"
 export MANPATH="$HOME/.linuxbrew/share/man:$MANPATH"
 export INFOPATH="$HOME/.linuxbrew/share/info:$INFOPATH"
